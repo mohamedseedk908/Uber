7a491c2817cef25d0b63b474c96e0ec5d7de53bc
Mohamed Seedyk <mohamedseedk908@gmail.com>
2024-11-18 15:12:16 -0800

    add splash screen

--- lib/screens/app_splash_screens.dart
+++ lib/screens/app_splash_screens.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+
+import 'main_screen.dart';
 
 class AppSplashScreen extends StatefulWidget {
   const AppSplashScreen({super.key});
@@ -9,10 +13,44 @@ class AppSplashScreen extends StatefulWidget {
 
 class _AppSplashScreenState extends State<AppSplashScreen> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    startSplashTimer();
+  }
+  startSplashTimer() async {
+    Timer(
+      const Duration(seconds: 4),
+      () => {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (_) => const MainScreen(),
+          ),
+        ),
+      },
+    );
+  }
+
+
+  @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.black,
-      child: Image.asset(""),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset("assets/images/splash.png"),
+          const SizedBox(
+            height: 25,
+          ),
+          Text(
+            "Uber Drivers App",
+            style: TextStyle(
+                color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
     );
   }
 }
