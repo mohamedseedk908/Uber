7ae627b59f5a75522116aac7357771c3e440a334
Mohamed Seedyk <mohamedseedk908@gmail.com>
2024-11-17 16:30:12 -0800

    add folder assets ans images

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:uber/screens/splash_screens.dart';
+import 'package:uber/screens/app_splash_screens.dart';
 
 void main() {
   runApp(const Uber());
@@ -13,7 +13,7 @@ class Uber extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      home: AppSplashScreen(),
     );
   }
 }
---
+++ lib/screens/app_splash_screens.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppSplashScreen extends StatefulWidget {
+  const AppSplashScreen({super.key});
+
+  @override
+  State<AppSplashScreen> createState() => _AppSplashScreenState();
+}
+
+class _AppSplashScreenState extends State<AppSplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+      child: Image.asset(""),
+    );
+  }
+}
